66fe1bdc85eccdddf4316a57d68e8ee9695c3408
Rosie Lickorish <rosielickorish@Rosies-MBP.home>
2016-03-26 19:02:52 +0000

    Rename chapter 1

---
+++ chapter_2/insertMany.js
@@ -0,0 +1,21 @@
+db.movies.remove([
+  {
+  "imbd": "tt0084726",
+  "title": "Star Wars II: The Wrath of Khan",
+  "year": 1982,
+  "type": "movie"
+  },
+  {
+  "imbd": "tt00796366",
+  "title": "Star Trek",
+  "year": 2009,
+  "type": "movie"
+  },
+  {
+  "imbd":"tt1308101",
+  "title": "Star Trek Into Darkness",
+  "year": 2013,
+  "type": "movie"
+  }
+]
+)
