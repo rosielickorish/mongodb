12fcef076d6a92a1079c826da7c21fb0e737aad2
Rosie Lickorish <rosielickorish@Rosies-MacBook-Pro.local>
2016-05-10 08:02:09 +0100

    Aggregation framework

---
+++ Aggregation Framewook/homework 1.js
@@ -0,0 +1,10 @@
+db.companies.aggregate( [
+    { $match: { "relationships.person.permalink": { $eq: "roger-ehrenberg" } } },
+    { $project: { relationships: 1, _id: 0 } },
+    { $unwind: "$relationships" },
+    { $group: {
+        _id: "$relationships.person",
+        count: { $sum: 1 }
+    } },
+    { $sort: { count: -1 } }
+] )
